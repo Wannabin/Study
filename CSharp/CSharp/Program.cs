2383ecb5befa65aa1d11382318e4784dcd528b68
Wannabin <wonbinkim91@gmail.com>
2021-04-14 21:40:59 +0900

    C

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp;
+            int level = 3^4;
+
+            hp = 100;
+
+            Console.WriteLine("Hello Number ! {0}", level);
+        }
+    }
+}
